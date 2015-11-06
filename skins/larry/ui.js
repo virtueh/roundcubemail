8978f5fb2cbff63039c7c064b9bc89e822e6efb6
virtueh <info@virtue.hk>
2015-11-06 16:11:54 +0800

    Update ui.js

--- skins/larry/ui.js
+++ skins/larry/ui.js
@@ -58,7 +58,8 @@ function rcube_mail_ui()
     }
 
     if (bw.tablet) {
-      $('#viewport').attr('content', "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0");
+      $('#viewport').attr('content', "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no"
+);
     }
   }
 
